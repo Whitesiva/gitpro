6192e420f0fa4c27080f0866ee5ca0fcf14107dc
Chann Krrish <chandrukrish48@gmail.com>
2020-02-24 17:36:24 +0530

    result created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
